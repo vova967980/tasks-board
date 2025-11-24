b8d7fe229444ce9e46d24f23c3a4db1c9ce128aa
Volodymyr Sirenko <vova967980@gmail.com>
2025-11-24 09:54:47 +0200

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svgr(), dts(), react()],
+  base: '/tasks-board/',
   resolve: {
     alias: {
       '@styles': path.resolve(__dirname, './src/styles'),
